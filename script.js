2ea76e84db4a156c278020efe244e1fdab923efe
trynil <ilovepudden@gmail.com>
2023-03-26 19:11:48 +0900

    clean and condense makeGrid script

--- script.js
+++ script.js
@@ -4,23 +4,16 @@ function makeGrid(){
     const grid = document.querySelector('.grid');
 
         const row = document.createElement('div');
-        row.classList.add(`row${i}`);
-        row.style.display = "flex";
-        row.style.width = "auto"
-        row.style.height = "auto"
-        row.style.flexDirection = "row"
+        row.classList.add("row",`${i}`);
+        row.style.cssText = "display: flex; flex-direction: row; width: auto; height: auto;"
 
         grid.appendChild(row);
             for (let c = 1; c < 17; c++){
-            const rows = document.querySelector(`.row${i}`);
+            const rows = document.querySelector(`div[class="row ${i}"]`);
         
                 const colum = document.createElement('div');
-                colum.classList.add(`colum${c}`);
-                colum.style.height = "50px"
-                colum.style.width = "50px"
-                colum.style.outlineStyle = "solid"
-                colum.style.outlineColor = "black"
-                colum.style.outlineWidth = "medium"
+                colum.classList.add("colum",`${c}`);
+                colum.style.cssText = " height: 50px; width: 50px; outline: black solid thin;"
         
                 rows.appendChild(colum);
                 
